7e45c8f579e1fb70f2f6bc2d10ab1f6a82900841
ilya lobanow <ilya1234567789@gmail.com>
2023-12-11 00:05:45 +1000

    Create Account, Contract, Card models

--- back/online_bank/main_app/models.py
+++ back/online_bank/main_app/models.py
@@ -3,9 +3,14 @@ from utils import custom_validators
 
 
 class User(models.Model):
+    class Sex(models.TextChoices):
+        MALE = 'male', 'мужской'
+        FEMALE = 'female', 'женский'
+
     first_name = models.TextField()
     second_name = models.TextField()
     third_name = models.TextField()
+    sex = models.TextField(choices=Sex.choices)
     phone_code = models.CharField(max_length=3, validators=[custom_validators.validate_phone_code])
     phone_number = models.CharField(max_length=14, validators=[custom_validators.validate_phone_number])
     email = models.EmailField(null=True)
@@ -16,4 +21,36 @@ class Authorization(models.Model):
     login = models.CharField(max_length=255, primary_key=True)
     password = models.BinaryField()
     salt = models.BinaryField()
-    user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='auth_user')
+    user = models.OneToOneField('User', on_delete=models.PROTECT, related_name='auth_user')
+
+
+class Account(models.Model):
+    class TypeAccount(models.TextChoices):
+        CREDIT = 'credit', 'кредитный'
+        DEBIT = 'debit', 'расcчётный'
+
+    type_account = models.TextField(choices=TypeAccount.choices)
+    account_number = models.CharField(max_length=20, unique=True,
+                                      validators=[custom_validators.exec_account_check_number])
+    currency = models.CharField(max_length=3)
+    balance = models.DecimalField()
+    user = models.ForeignKey('User', on_delete=models.PROTECT, related_name='accounts')
+
+
+class Contract(models.Model):
+    contract_number = models.TextField(unique=True)
+    description = models.JSONField()
+    account = models.OneToOneField('Account', on_delete=models.PROTECT, related_name='contract')
+
+
+class Card(models.Model):
+    card_number = models.CharField(max_length=16, primary_key=True,
+                                   validators=[custom_validators.exec_card_check_number])
+    init_datetime = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField()
+    is_activated = models.BooleanField()
+    token_card = models.UUIDField(unique=True)
+    pin_hash = models.BinaryField(null=True)
+    cvc_hash = models.BinaryField()
+    account = models.ForeignKey('Account', on_delete=models.PROTECT, related_name='cards')
+
